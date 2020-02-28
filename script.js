4a970c696175bc8b0ae713244d63a51213f23ffa
Juhana Viitamo <juhana.viitamo@aalto.fi>
2020-02-28 13:49:35 +0200

    mobiili valikko

--- script.js
+++ script.js
@@ -19,4 +19,13 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
 
